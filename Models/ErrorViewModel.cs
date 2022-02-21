26631951f94e39ea92e5c9111eb3f5abb63a86f4
yamen Aln <yamen.alns@gmail.com>
2022-02-21 14:49:12 +0100

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ithsgithubcicd.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
